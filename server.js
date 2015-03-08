42d28ded1632032f83a02464debb98c23d877208
Jagdeep Matharu <Jagdeepmatharu88@gmail.com>
2015-03-08 13:25:30 -0400

    Fixing Chat issue

--- server.js
+++ server.js
@@ -3,16 +3,14 @@ var express = require('express'),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server);
 
-app.listen(3000, function() {
-    console.log('Server is Running at http://localhost:3000/');
-});
+server.listen(3000);
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/public/index.html');
+    res.sendfile(__dirname + '/public/index.html');
 });
 
-io.socket.on('connection', function(socket){
-	socket.on('send message', function(data){
-		io.socket.emit('new message', data);
-	});
+io.sockets.on('connection', function(socket) {
+    socket.on('send message', function(data) {
+        io.sockets.emit('new message', data);
+    });
 });
